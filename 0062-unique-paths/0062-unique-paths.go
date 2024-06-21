8b07f8d51d4ee8b921e6523e7e1bb131d9fe0b89
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-06-21 23:16:02 +0700

    Time: 7 ms (10.23%), Space: 3.7 MB (6.05%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.go
@@ -0,0 +1,20 @@
+func uniquePaths(m int, n int) int {
+    dp := make(map[string]int)
+    for i := 0; i < m; i ++ {
+        for j := 0; j < n; j++ {
+            if i == 0 || j == 0 {
+                dp[key(i,j)] = 1
+                continue
+            }
+            
+            dp[key(i,j)] = dp[key(i-1, j)] + dp[key(i, j-1)]
+        }
+    }
+
+    fmt.Println(dp)
+    return dp[key(m-1, n-1)]
+}
+
+func key(i, j int) string {
+    return fmt.Sprintf("%d%d", i, j)
+}
