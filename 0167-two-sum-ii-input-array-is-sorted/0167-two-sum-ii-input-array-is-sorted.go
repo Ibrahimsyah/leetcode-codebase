adbb7d8fdfd5d262335cefb3dd3d0aa2542f4d1b
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-06-25 22:54:40 +0700

    Time: 11 ms (50.86%), Space: 6.1 MB (67.88%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.go
@@ -2,11 +2,12 @@ func twoSum(numbers []int, target int) []int {
     l, r := 0, len(numbers) - 1
 
     for l < r {
-        if numbers[l] + numbers[r] == target {
+        result := numbers[l] + numbers[r]
+        if result == target {
             return []int{l + 1, r + 1}
         }
 
-        if numbers[l] + numbers[r] < target {
+        if result < target {
             l ++
         } else {
             r -- 
