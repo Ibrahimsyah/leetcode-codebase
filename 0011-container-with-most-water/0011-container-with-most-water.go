eeaa33eba49879e37b68e1cb582074644ebfaeb2
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-06-26 18:23:13 +0700

    Time: 74 ms (29.17%), Space: 13.2 MB (5.16%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.go
+++ 0011-container-with-most-water/0011-container-with-most-water.go
@@ -2,9 +2,8 @@ func maxArea(height []int) int {
     l, r := 0, len(height) - 1
     maxArea := 0
     for l < r {
-        area := (r - l) * min(height[l], height[r])
-        maxArea = max(maxArea, area)
-        if height[l] <= height[r] {
+        maxArea = max(maxArea, min(height[l], height[r]) * (r - l))
+        if height[l] < height[r] {
             l++
         } else {
             r--
