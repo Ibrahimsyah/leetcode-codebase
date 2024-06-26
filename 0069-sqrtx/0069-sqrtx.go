bc1715ce0786cbc0748a78f290e000b71bcd0da3
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-06-26 17:22:22 +0700

    Time: 2 ms (54.38%), Space: 2.3 MB (25.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.go
+++ 0069-sqrtx/0069-sqrtx.go
@@ -2,16 +2,15 @@ func mySqrt(x int) int {
     l, r := 1, x
     for l <= r {
         m := l + (r - l) / 2
-        sqrt := x / m
-        if m > sqrt {
+        squared := m * m
+        if squared < x {
+            l = m + 1
+        } else if squared > x {
             r = m - 1
         } else {
-            if m + 1 > x / (m + 1) {
-                return m
-            }
-            l = m + 1
+            return m
         }
     }
 
-    return 0
+    return r
 }
