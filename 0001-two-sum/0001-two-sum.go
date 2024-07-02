9f6e4212a9f093310b457bb0a780ce3eaa48781a
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-07-02 08:13:01 +0700

    Time: 6 ms (68.65%), Space: 4.2 MB (57.87%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,9 +1,10 @@
 func twoSum(nums []int, target int) []int {
     m := make(map[int]int)
     for i, n := range nums {
-        if index, f := m[target-n]; f {
-            return []int{index, i}
+        if rI, f := m[target - n]; f {
+            return []int{i, rI}
         }
+
         m[n] = i
     }
 
