33f08a1beb21371e036d241694fd1173d8c5825d
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-07-02 10:05:12 +0700

    Time: 5 ms (56.68%), Space: 3 MB (91.14%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.go
+++ 0013-roman-to-integer/0013-roman-to-integer.go
@@ -1,61 +1,25 @@
-var romanMap = map[string]int {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
-}
-
 func romanToInt(s string) int {
-    result := 0
-    index := 0
-    for index < len(s){
-        i := index
-        index++
-        char := string(s[i])
-        if i == len(s) - 1 {
-            result += romanMap[char] 
-            break
-        }
-        switch char {
-            case "I":
-                if string(s[i+1]) == "V" {
-                    result += 4
-                    index++
-                    continue
-                }
-                if string(s[i+1]) == "X" {
-                    result += 9
-                    index++
-                    continue
-                }
-            case "X":
-                if string(s[i+1]) == "L" {
-                    result += 40
-                    index++
-                    continue
-                }
-                if string(s[i+1]) == "C" {
-                    result += 90
-                    index++
-                    continue
-                }
-            case "C":
-                if string(s[i+1]) == "D" {
-                    result += 400
-                    index++
-                    continue
-                }
-                if string(s[i+1]) == "M" {
-                    result += 900
-                    index++
-                    continue
-                }
-        }
-        result += romanMap[char] 
+    m := map[string]int{
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000,
     }
 
-    return result
+    res := 0
+    i := 0
+    for i < len(s) {
+        if i < len(s) - 1 && m[string(s[i])] < m[string(s[i+1])]{
+            res += m[string(s[i+1])] - m[string(s[i])]
+            i += 2
+        } else {
+            res += m[string(s[i])]
+            i++
+        }
+    }
+
+    return res
 }
