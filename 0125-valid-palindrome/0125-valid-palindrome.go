e4be1cd47f349dc31f219e05138b27cb096a1a23
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-07-02 10:50:11 +0700

    Time: 0 ms (100%), Space: 3 MB (70.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.go
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -1,10 +1,35 @@
 func isPalindrome(s string) bool {
-    r := regexp.MustCompile("[^a-zA-Z0-9]+")
-    ss := r.ReplaceAllString(strings.ToLower(s), "")
-    for i := 0; i < len(ss)/2; i++ {
-        if ss[i] != ss[len(ss)-i-1] {
+    s = strings.ToLower(s)
+    l, r := 0, len(s) - 1
+    for l < r {
+        if !isAlphanumeric(s[l]) {
+            l++
+            continue
+        }
+
+        if !isAlphanumeric(s[r]) {
+            r--
+            continue
+        }
+        if s[l] != s[r] {
             return false
         }
+
+        l++
+        r--
     }
+    
     return true
 }
+
+func isAlphanumeric(s byte) bool {
+    if s >= 97 && s <= 122 {
+        return true
+    }
+
+    if s >= 48 && s <= 57 {
+        return true
+    }
+
+    return false
+} 
