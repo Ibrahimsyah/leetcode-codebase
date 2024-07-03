013089fae7f43e08162806ca85868c9b15afb6d6
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-07-03 09:17:11 +0700

    Time: 4 ms (59.1%), Space: 5.1 MB (5.67%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.go
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func hasPathSum(root *TreeNode, targetSum int) bool {
+    if root == nil {
+        return false
+    }
+
+    res := false
+    var dfs func(n *TreeNode, sum int)
+    dfs = func(n *TreeNode, sum int) {
+        sum += n.Val
+        if n.Left == nil && n.Right == nil {
+            fmt.Println(n, sum)
+            if sum == targetSum {
+                res = true
+            }
+            return
+        }
+
+        if n.Left != nil {
+            dfs(n.Left, sum)
+        }
+
+        if n.Right != nil {
+            dfs(n.Right, sum)
+        }
+    }
+
+    dfs(root, 0)
+
+    return res
+}
