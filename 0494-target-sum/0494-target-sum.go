dcd6d1c7be9eac786626525c548fe694661f922e
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2024-07-30 09:07:44 +0700

    Time: 157 ms (41.41%), Space: 7.2 MB (42.58%) - LeetHub

--- 0494-target-sum/0494-target-sum.go
+++ 0494-target-sum/0494-target-sum.go
@@ -1,4 +1,5 @@
 func findTargetSumWays(nums []int, target int) int {
+    m := make(map[string]int)
     var solve func(index, total int) int
     solve = func(index, total int) int {
         if index == len(nums) {
@@ -7,9 +8,18 @@ func findTargetSumWays(nums []int, target int) int {
             }
             return 0
         }
+        dpKey := key(index, total)
+        if v, f := m[dpKey]; f {
+            return v
+        }
 
-        return solve(index + 1, total + nums[index]) + solve(index + 1, total - nums[index])
+        m[dpKey] = solve(index + 1, total + nums[index]) + solve(index + 1, total - nums[index])
+        return m[dpKey]
     }
 
     return solve(0, 0)
 } 
+
+func key(index, total int) string {
+    return fmt.Sprintf("%d-%d", index, total)
+}
